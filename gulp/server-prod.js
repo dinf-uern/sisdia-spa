a8f40495711689bec1309a12bae880be551b3a11
Luis <luisinweb@gmail.com>
2016-01-20 09:53:40 -0300

    modificado para servir a página usando express com o módulo compress

--- gulp/server-prod.js
+++ gulp/server-prod.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var express = require('express');
+var compress = require('compression');
+var app = express();
+var port = process.env.PORT || 3000;
 
 gulp.task('serve:prod', ['build', 'copy:icons'], function() {
-  connect.server({
-    root: ['./dist'],
-    port: process.env.PORT || 8080, // localhost:5000
-    livereload: false
+  app.use(compress());
+  app.use(express.static('./dist'));
+
+  app.listen(port, function () {
+    console.log('Escutando na porta ' + port);
   });
 });
